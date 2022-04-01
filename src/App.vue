1d2b540ad04edb986e4d4783387fc69761c38cdd
hkopaa <tixiibred@gmail.com>
2022-04-01 10:29:47 +0300

    Start point 1.1

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,6 @@ export default defineComponent({});
   margin: 0;
 }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
   padding: 30px;
 
